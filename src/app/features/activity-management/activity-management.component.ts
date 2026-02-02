93498b35633f990a358b7bd63ac7c059eca7e882
salma <salmafarouk5@outlook.com>
2026-02-02 12:06:32 +0400

    Adding 2nd page

---
+++ src/app/features/activity-management/activity-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-management.component.html',
+  styleUrl: './activity-management.component.css'
+})
+export class ActivityManagementComponent {
+
+}
